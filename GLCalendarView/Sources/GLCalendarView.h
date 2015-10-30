360443d3f2d16792f12278afdafdfaa753a9559e
john zuo <johnzuo305@gmail.com>
2015-10-30 17:09:46 +1100

    1.8.0

--- GLCalendarView/Sources/GLCalendarView.h
+++ GLCalendarView/Sources/GLCalendarView.h
@@ -18,10 +18,10 @@
 - (void)calenderView:(GLCalendarView *)calendarView finishEditRange:(GLCalendarDateRange *)range continueEditing:(BOOL)continueEditing;
 - (BOOL)calenderView:(GLCalendarView *)calendarView canUpdateRange:(GLCalendarDateRange *)range toBeginDate:(NSDate *)beginDate endDate:(NSDate *)endDate;
 - (void)calenderView:(GLCalendarView *)calendarView didUpdateRange:(GLCalendarDateRange *)range toBeginDate:(NSDate *)beginDate endDate:(NSDate *)endDate;
+- (NSString*)calenderView:(GLCalendarView *)calendarView TiShowByBeginDate:(NSDate*)begindate endDate:(NSDate*)enddate;
 
 @optional
 - (NSArray *)weekDayTitlesForCalendarView:(GLCalendarView *)calendarView;
-- (NSString*)calenderView:(GLCalendarView *)calendarView TiShowByBeginDate:(NSDate*)begindate endDate:(NSDate*)enddate;
 @end
 
 
